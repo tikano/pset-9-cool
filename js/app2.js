e6cae4f3cbc750527a06ae768a256d4bc18e2333
tikano <oadmathgenie@gmail.com>
2020-02-11 13:12:54 -0500

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,6 +1,6 @@
 window.onload = function(){
 console.log("hey");
 for (var i=0; i< 64; i++){
-    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
+    document.getElementById("mainChessBoard").appendChild(document.createElement("div class = ".square"")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
 }
 }
