4b5252000e0311cfec7e29215bc98b94bcd440e7
tikano <oadmathgenie@gmail.com>
2020-02-11 12:59:43 -0500

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,15 +1,3 @@
-var table = document.createElement("table");
-    for (var i = 1; i < 9; i++) {
-    var tr = document.createElement('tr');
-    for (var j = 1; j < 9; j++) {
-        var td = document.createElement('td');
-        if (i%2 == j%2) {
-            td.className = "white";
-        } else {
-            td.className = "black";
-        }
-        tr.appendChild(td);
-    }
-    table.appendChild(tr);
+for (var i=0; i< 64; i++){
+    document.getElementById("mainChessBoard").appendChild(document.createElement("div")).style.backgroundColor = parseInt((i / 8) + i) % 2 == 0 ? '#ababab' : 'white';    
 }
-document.body.appendChild(table);
